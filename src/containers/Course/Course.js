81564061bfcc76ff2d673b0c59566c5a374b689d
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-19 14:52:35 -0700

    Finished exercise with extra credit.

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 class Course extends Component {
     render () {
+        const paramsString = this.props.location.search;
+        const searchParams = new URLSearchParams(paramsString);
+        let title = [];
+        for(let p of searchParams) {
+            title.push(p);
+        }
+        
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{title[0][1]}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
